7871ac91019ca2c8d3a662f81008fa63254fd3b5
Sallyalzoubi02 <alzoubisally0@gmail.com>
2025-03-13 10:56:13 +0300

    c

--- angularapp1.client/src/app/app-routing.module.ts
+++ angularapp1.client/src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { CategoryComponent } from './category/category.component';
 import { ProductComponent } from './product/product.component';
 
 const routes: Routes = [
-  { path: 'nav-bar', component: NavBarComponent },
   { path: 'home', component: HomeComponent },
   { path: 'sign-in', component: SignInComponent },
   { path: 'sign-up', component: SignUpComponent },
